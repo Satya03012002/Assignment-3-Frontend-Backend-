d56a107ce5ee7c416bc00659d1a68539a35cfce5
satya prakash <satya123.sp34@gmail.com>
2023-04-06 20:40:17 +0530

    final

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {  Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 
 
