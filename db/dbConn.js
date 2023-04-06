2648846bec408ce751047097bac6701a82cb30b6
satya prakash <satya123.sp34@gmail.com>
2023-04-06 18:03:16 +0530

    final

--- db/dbConn.js
+++ db/dbConn.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ silent: process.env.NODE_ENV === 'production' });
-const CONNECTION_URL = `mongodb+srv://satya123sp34:0Wk6XeZ1cO0Kkjkk@cluster0.lzsnv13.mongodb.net/?retryWrites=true&w=majority`
+const CONNECTION_URL = `mongodb+srv://${process.env.DBNAME}:${process.env.PASSWORD}@cluster0.lzsnv13.mongodb.net/?retryWrites=true&w=majority`
 mongoose.set("strictQuery", false);
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("mongodb connected successfully"))
