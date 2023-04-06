6d0699588e9a3acf9e4daec796b364c93e82b9e9
satya prakash <satya123.sp34@gmail.com>
2023-04-06 17:48:55 +0530

    final

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 import "./db/dbConn.js";
 import cors from 'cors';
 import SignInRouter from "./router/loginRequest/router.js";
@@ -10,6 +11,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/signin",SignInRouter);
+
+// seving thee frontend
+
+app.use(express.static(path.join(__dirname,"./client/build")))
+app.get("*", function(_,res){
+    res.sendFile(
+        path.join(__dirname,"/client/build/index.html"),(err)=>{
+            res.status(500).send(err)
+        }
+    )
+})
 
 app.listen(PORT,()=>{
     console.log(`server running successfully on PORT : ${PORT}`)
